92b9d723b8c1b028aa07ba18a54e5ef54543ab1a
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-03-20 18:38:05 +0100

    fix: scrollbar hiden

--- src/app/dashboard/learning/wish-list/page.tsx
+++ src/app/dashboard/learning/wish-list/page.tsx
@@ -3,10 +3,6 @@ import { CourseCardStatOverView } from "../components/course-card";
 function Page() {
   return (
     <section>
-      <div className="flex justify-between items-center w-[90%]">
-        <h1 className="text-2xl font-semibold">Courses you might like</h1>
-        <button className="font-medium text-sm">Load All</button>
-      </div>
       <div className="flex flex-wrap justify-between gap-4 2 w-[90%]">
         <CourseCardStatOverView />
         <CourseCardStatOverView />
