e58a26a2fa81fd375f182e7f307bc37e81baa7c3
“Akshola00” <akinniyishola07@gmail.com>
2025-03-20 15:48:28 +0100

    feat: implement base tutor page

--- src/app/dashboard/tutor/page.tsx
+++ src/app/dashboard/tutor/page.tsx
@@ -1,8 +1,25 @@
-function Page(){
-    return(
-        <h1>Begin</h1>
-    )
-}
+"use client";
+import { useContext } from "react";
+import { DashBoardContext } from "@/app/useContext/dashboardContext";
+import Tutor from "./tutor/tutor";
+import Student from "./students/students";
+import Notification from "./notification/notification";
+import Profile from "./profile/profile";
 
+function Page() {
+  const { activeSection } = useContext(DashBoardContext);
+
+  return (
+    <div className="overflow-y-auto scrollbar-hide scroll-smooth">
+      {activeSection === "home" && <Tutor />}
+      {activeSection === "students" && <Student />}
+      {activeSection === "notifications" && <Notification />}
+      {activeSection === "courses" && "courses"}
+      {activeSection === "earnings" && "earnings"}
+      {activeSection === "support" && "support"}
+      {activeSection === "profile" && <Profile />}
+    </div>
+  );
+}
 
 export default Page;
--- src/components/WalletConnectModal.tsx
+++ src/components/WalletConnectModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button } from "../components/ui/Button";
+import { Button } from "../components/ui/button";
 import Image from "next/image";
 import { useWalletContext } from "../useContext/WalletContext";
 import ArgentImg from "../../public/svg/ArgentImg.svg";
